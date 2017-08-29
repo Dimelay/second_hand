6cc40af5c936b7e41f26785ca45167371e955161
steck <steck@Work>
2017-08-29 21:36:06 +0800

    new

--- product/forms.py
+++ product/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
 from product.models import discont
+from django.utils import timezone
 
 class discont_form(forms.Form):
    # discont = forms.ModelChoiceField(query=discont.objects.all(), widget=forms.RadioSelect)
@@ -10,7 +11,7 @@ class discont_form(forms.Form):
 class date_input(forms.Form):
     class Media:
         js = ('js/print_barcode.js',)
-    date_input = forms.DateField(label=u'Дата ', widget=SelectDateWidget(attrs={'class':'date_input'},years=range(2016, 2030)))
+    date_input = forms.DateField(initial=timezone.now,label=u'Дата ', widget=SelectDateWidget(attrs={'class':'date_input'},years=range(2016, 2030)))
 class SearchPoint(forms.Form):
 
 	class Media:
