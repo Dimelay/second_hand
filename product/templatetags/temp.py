bf547ab03f9ccbd6dd3c9d8d9a5b45e7387724e1
steck <steck@Work>
2017-09-21 11:00:44 +0800

    Переработана история

--- product/templatetags/temp.py
+++ product/templatetags/temp.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
 from django import template
+import datetime
 from django.utils.safestring import mark_safe
+from collections import OrderedDict
 register = template.Library()
 
-@register.filter
-def get_summ(log):
-    stat = {}
-    stat['money_in']=0
-    stat['money_out']=0
-    stat['money_all']=0
+@register.inclusion_tag('history_result_tag.html')
+def history_result(log):
+    all_stat = {}
+    all_money_in=0
+    all_money_out=0
+    all_money_all=0
     for i in log:
-        stat['money_in'] = stat['money_in']+i.money_in
-        stat['money_out'] = stat['money_out']+i.money_out
-        stat['money_all'] = stat['money_in']-stat['money_out']
-    """
-        if s=='in':
-            a = a+i.money_in
-        elif s=='out':
-            a = a+i.money_out
+        all_money_in+=i.money_in
+        all_money_out+=i.money_out
+        d_date = datetime.datetime.strftime(i.dates.date(),"%Y-%m-%d")
+        if d_date in all_stat:
+            all_stat[d_date]['money_in']+=i.money_in
+            all_stat[d_date]['money_out']+=i.money_out
+            all_stat[d_date]['money_all']+=(i.money_in - i.money_out)
         else:
-            r_in = r_in+i.money_in
-            r_out = r_out+i.money_out
-            a = r_in - r_out
-    """
-    return mark_safe(('Приход: <b>%s</b> Расход: <b>%s</b> Итого: <b>%s</b>') % (stat['money_in'],stat['money_out'],stat['money_all']))
+            all_stat[d_date]=dict(money_in=i.money_in,money_out=i.money_out,money_all=i.money_in-i.money_out)
+    all_money_all = all_money_in - all_money_out
+    all_stat[None]=dict(money_in=all_money_in,money_out=all_money_out,money_all=all_money_all)
+    a=sorted(all_stat.items(), key=lambda x: x[0])
+    return {'stat':a}
+
+@register.filter
+def to_datetime(val):
+    try:
+        return datetime.datetime.strptime(val,"%Y-%m-%d").date()
+    except:
+        return None
